96b52e57fe0ab39bb2f2e589f3c41654316eaa1f
Pranav <pranavrudra1999@gmail.com>
2023-11-07 16:25:04 -0500

    Implemented sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from '@mantine/core'
 import '@mantine/core/styles.css';
-import './App.css';
+import './scss/main.scss';
 
 function App() {
   const [buttonKey, setButtonKey] = useState(0)
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='full-height center'>
       <div>
-        <Button onClick={()=>{setButtonKey(buttonKey >= buttonContet.length - 1 ? 0 : buttonKey + 1)}}>{buttonContet[buttonKey]}</Button>
+        <Button onClick={() => { setButtonKey(buttonKey >= buttonContet.length - 1 ? 0 : buttonKey + 1) }}>{buttonContet[buttonKey]}</Button>
       </div>
     </div>
   );
