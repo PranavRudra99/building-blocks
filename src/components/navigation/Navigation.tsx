ab0e6229398e58fc5d6d72e52e3fec2155493181
Pranav <pranavrudra1999@gmail.com>
2023-11-08 00:38:48 -0500

    Added basic navigation functionality

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,16 +1,20 @@
 import { Menu, Burger } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import { paths } from '../../utilities/constants'
+import { useNavigate } from 'react-router'
 
 const Navigation = (): JSX.Element => {
+    const [opened, { toggle }] = useDisclosure()
+    const navigate = useNavigate()
     return (
         <Menu>
             <Menu.Target>
-                <Burger></Burger>
+                <Burger opened={opened} onClick={toggle}></Burger>
             </Menu.Target>
             <Menu.Dropdown>
                 {paths.map((path)=>{
                     return (
-                        <Menu.Item leftSection={<path.icon />}>{path.label}</Menu.Item>
+                        <Menu.Item leftSection={<path.icon />} onClick={()=>{toggle(); navigate(path.path)}}>{path.label}</Menu.Item>
                     )
                 })}
             </Menu.Dropdown>
